db0f037928847a4acbc1d45ae9b2868c1a95052c
xuda <xuda.it@outlook.com>
2019-05-18 22:48:54 +0800

    项目构建

---
+++ src/main/java/org/virtue/FastSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.virtue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastSpringbootApplication.class, args);
+    }
+
+}
